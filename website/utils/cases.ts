c191dae3a4ffbdd1b344f6da8f856cda94cc6b29
Fabio Mughilan <fabiomughilan@gmail.com>
2025-02-09 20:56:03 +0530

    utils

---
+++ website/utils/cases.ts
@@ -0,0 +1,44 @@
+import { Case } from "@/types/case";
+
+export const activeCases: Case[] = [
+  {
+    id: '1',
+    title: 'Scam Case',
+    status: 'active',
+    dateCreated: '2025-01-11',
+    priority: 'high',
+    roomId: '6fa8055f-4f5a-48b5-89b5-4e8a07dade2a'
+  },
+  {
+    id: '2',
+    title: 'Employment Dispute',
+    status: 'active',
+    dateCreated: '2025-01-10',
+    priority: 'medium',
+    roomId: '00cf82a3-37f2-4969-b5c4-d9a9c4ca90db'
+  },
+  {
+    id: '3',
+    title: 'Property Agreement',
+    status: 'active',
+    dateCreated: '2025-01-09',
+    priority: 'low',
+    roomId: '933dd399-138e-4dcf-aa8a-371698943ea1'
+  },
+  {
+    id: '4',
+    title: 'Intellectual Property Case',
+    status: 'active',
+    dateCreated: '2025-01-08',
+    priority: 'high',
+    roomId: '6453fea2-90b3-4a80-b030-0587c7104c81'
+  },
+  {
+    id: '5',
+    title: 'Intellectual Property Case',
+    status: 'active',
+    dateCreated: '2025-01-08',
+    priority: 'high',
+    roomId: 'c7b0b0ee-b2cb-42db-9ef5-b8b83e45c4e3'
+  },
+]
